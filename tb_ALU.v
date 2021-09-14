3a203a65bfcd0002e3ee905eaf5fc4fbf4c085ac
Elmir Dzaka <elmir.dzaka@utah.edu>
2021-09-13 18:19:57 -0600

    Updated tests

--- tb_ALU.v
+++ tb_ALU.v
@@ -4,10 +4,10 @@ module tb_ALU;
     reg [15:0] tb_r2;
     reg [7:0] tb_opcode;
 	 reg tb_clock;
-    wire [15:0] tb_rout;
+    wire [15:0] rout_out;
 	 integer i,j;
 
-    ALU uut(.r1(tb_r1), .r2(tb_r2), .opcode(tb_opcode), .rout(tb_rout));
+    ALU uut(.r1(tb_r1), .r2(tb_r2), .opcode(tb_opcode), .rout(rout_out));
     
     always begin
         #10;
@@ -32,20 +32,20 @@ module tb_ALU;
         #20 //wait 20 seconds
             $display("1.", "rout = ", rout_out);
             
-        //mult
-        tb_r1 = 1; tb_r2 = 1; tb_opcode = 8'b00001110;
-        #20 //wait 20 seconds
-            $display("1.", "rout = ", rout_out);
+//        //mult
+//        tb_r1 = 1; tb_r2 = 1; tb_opcode = 8'b00001110;
+//        #20 //wait 20 seconds
+//            $display("1.", "rout = ", rout_out);
             
         //sub
         tb_r1 = 1; tb_r2 = 1; tb_opcode = 8'b00001001;
         #20 //wait 20 seconds
             $display("1.", "rout = ", rout_out);
             
-        //subc
-        tb_r1 = 1; tb_r2 = 1; tb_opcode = 8'b00001010;
-        #20 //wait 20 seconds
-            $display("1.", "rout = ", rout_out);
+//        //subc
+//        tb_r1 = 1; tb_r2 = 1; tb_opcode = 8'b00001010;
+//        #20 //wait 20 seconds
+//            $display("1.", "rout = ", rout_out);
             
         
         //cmp
@@ -75,41 +75,36 @@ module tb_ALU;
         #20 //wait 20 seconds
             $display("1.", "rout = ", rout_out);
             
-        //ashu
-        tb_r1 = 1; tb_r2 = 1; tb_opcode = 8'b10000110;
+			//RSH
+        tb_r1 = 1; tb_r2 = 1; tb_opcode = 8'b00001000;
         #20 //wait 20 seconds
             $display("1.", "rout = ", rout_out);
-            
+				
+        //ALSH
+        tb_r1 = 1; tb_r2 = 1; tb_opcode = 8'b00001100;
+        #20 //wait 20 seconds
+            $display("1.", "rout = ", rout_out);
+				
+         //ARSH
+        tb_r1 = 1; tb_r2 = 1; tb_opcode = 8'b00001111;
+        #20 //wait 20 seconds
+            $display("1.", "rout = ", rout_out);
         
-            
-            
+			//not
+        tb_r1 = 1; tb_r2 = 1; tb_opcode = 8'b00000100;
+        #20 //wait 20 seconds
+            $display("1.", "rout = ", rout_out);
         
             //testing add
-        for(i=0; i< 65535; i=i+1) 
-            begin
-                for(j = 0; j> 65535; i= i+1) 
-                    begin
-                        tb_r1 = i; tb_r2 = j; tb_opcode = 8'b00000101;
-                        #20 //wait 20 seconds
-                            $display("1.", "rout = ", rout_out);
-                    end
-            end
+//        for(i=0; i< 65535; i=i+1) 
+//            begin
+//                for(j = 0; j> 65535; i= i+1) 
+//                    begin
+//                        tb_r1 = i; tb_r2 = j; tb_opcode = 8'b00000101;
+//                        #20 //wait 20 seconds
+//                            $display("1.", "rout = ", rout_out);
+//                    end
+           end
             
-            
-            
-            //testing addu
-        for(i=0; i< 65535; i=i+1) 
-            begin
-                for(j = 0; j> 65535; j= j+1) 
-                    begin
-                        tb_r1 = i; tb_r2 = j; tb_opcode = 8'b00000110;
-                        #20 //wait 20 seconds
-                            $display("1.", "rout = ", rout_out);
-							end
-             end
-        
-    
-    
-			end    
  endmodule
  
