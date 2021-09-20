07bf1061b96b95e646b8f57c708b89c440da693f
Elmir Dzaka <elmir.dzaka@utah.edu>
2021-09-20 15:29:50 -0600

    Updated ALU

--- ALU.v
+++ ALU.v
@@ -6,60 +6,60 @@ input [15:0] r2;
 
 output reg [15:0] rout;
 input [7:0] opcode;
-reg [7:0] flag = 7'b0000000; //C == 0, L == 2, F == 5(OF), Z == 6, N == 7
+reg [7:0] flag = 8'b00000000; //C == 0, L == 2, F == 5(OF), Z == 6, N == 7
 
 always @(r1, r2, opcode)
 	begin 
 		case(opcode)
 			8'b00000101 : begin
-			flag = 7'b0000000; //add
+				flag = 8'b00000000; //add
 				{flag[0], rout} = r1 + r2;
 				if(r2[15] == r1[15] && rout[15] != r1[15]) begin
-					flag[5] = 1;	
+					flag = 8'b00100000;	
 				end	
 					else begin
-						flag[5] = 0;
+						flag = 8'b00100000;
 					end
 			
 			 end 
          8'b00000110 : rout = r1 + r2; //addu
          8'b00000111 : rout = r1 + r2 + flag[0]; //addc
          8'b00001001 : begin
-			flag = 7'b0000000;
+				flag = 8'b00000000;
 				{flag[0],rout} = r1 + (~r2 + 1); //sub
 				
 				if((~r2[15] + 1) == r1[15] && rout[15] != r1[15]) begin
-					flag[5] = 1;	
+					flag = 8'b00100000;	
 				end	
 					else begin
-						flag[5] = 0;
+						flag = 8'b00000000;  
 					end
 			end
 			
          8'b00001011 : begin //cmp
-				flag = 7'b0000000;
+				flag = 8'b00000000;
 
 				rout = r1 - r2;
 				
 				if(r1 == r2) begin 
-						flag[6] = 1;
+						flag = 8'b01000000;
 				end
 		   	else begin
-					flag[6] = 0;
+					flag = 8'b00000000;
 					if(($signed(r1) < 0 || $signed(r2) < 0) || ($signed(r1) < 0 && $signed(r2) < 0)) begin			
 						if($signed(rout) < $signed(r2)) begin
-							flag[7] = 1;
+							flag = 8'b10000000;
 						end
 						else begin
-							flag[7] = 0;
+							flag = 8'b00000000;
 						end
 					end
 					else begin
 						if($signed(rout) < $signed(r2)) begin
-							flag[2] = 1;
+							flag = 8'b10000100;
 						end
 						else begin
-							flag[2] = 0;
+							flag = 8'b10000000;
 						end
 					end
 				end
